4c91e999eaa33b3aef2a4c1c8d95d798468f4b9d
Felipe Dias Feitosa <dias.pesca@gmail.com>
2026-08-28 20:53:04 -0300

    Add constants and configuration

---
+++ cnae_etc/constants.py
@@ -0,0 +1,19 @@
+# Set of aquaculture interest CNAEs
+# Conjunto de CNAEs de interesse para aquicultura
+AQUACULTURE_CNAES = {'0321301','0321302','0321303','0321304','0321305','0321399','0322101','0322102','0322103','0322104','0322105','0322106','0322107','0322199'}
+
+# Target columns in the bigdata that has CNAEs
+# Colunas alvo no bigdata que contém CNAEs
+TARGET_COLUMNS = [11,12]
+
+# Column delimeter
+# Delimitador de colunas 
+SEP = ';'
+
+# Bigdata's file encoding
+# Codificação do arquivo bigdata
+ENCODING = 'latin1'
+
+# Data slice size processed each time
+# Tamanho da fatia de dados processada por vez
+CHUNKSIZE = 500000
